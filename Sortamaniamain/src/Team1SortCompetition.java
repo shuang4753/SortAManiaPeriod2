ebe06481fe4850f1e7cbdb081e5c4584c997925f
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2018-02-26 09:28:07 -0500

    Added Team1 Greeting

---
+++ Sortamaniamain/src/Team1SortCompetition.java
@@ -0,0 +1,8 @@
+
+public class Team1SortCompetition extends SortCompetition {
+
+	public String greeting()
+	{return "This is Team 1,Tyson Lawrence and Amy Chen. We are ready to compete!";
+		
+	}
+}
