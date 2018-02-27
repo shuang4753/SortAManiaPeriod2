0e88b02827dff72c2f6e52d34f4034068d5db379
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-27 09:07:24 -0500

    ss

--- Sortamaniamain/src/Team2.java
+++ Sortamaniamain/src/Team2.java
@@ -1,4 +1,30 @@
 
 public class Team2 {
-a
+
+	
+	//challengeOne: Standard sort and process
+	public static int challengeOne(int[] list1) 
+	{
+		//random number generator 
+		//an array of 10,000 random integers between 0-1000
+		int temp=0;
+		for(int outside=1; outside<list1.length; outside++)
+		{
+			for(int inside=outside; inside>0; inside--)
+			{
+				if(list1[inside]<list1[inside-1])
+				{
+					temp=list1[inside-1];
+					list1[inside-1]=list1[inside];
+					list1[inside]=temp;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}	
+		
+		return (list1.length/2)+1;
+	}
 }
