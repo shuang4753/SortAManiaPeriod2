018df124c3f670226c0728537b4a7da9e8345ec0
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-27 09:22:49 -0500

    start test

--- Sortamaniamain/src/Team2.java
+++ Sortamaniamain/src/Team2.java
@@ -1,11 +1,35 @@
-//Lingli, Alyssa, Lily
-//2/27/2018
+/**
+ * SortAMania
+ * @author Lingli Zou, Alyssa Ma, Lily Li
+ * Period 2
+ * 2/26/18
+ */
 
 public class Team2 {
 
 	public class Team2 extends SortCompetition
 	{
-		
+		public class Team2 extends SortCompetition
+		{
+			//Testing Code
+			public static void main(String [] args)
+			{
+				
+				//Challenge 1 10000 Rand Int Test
+				int int1000 = Math.random()*10001;
+				long start = System.nanoTime();
+				challengeOne();
+				long end = System.nanoTime();
+				long time = end - start;
+				
+				//Challenge 2 10000 Rand String Test
+				
+				//Challenge 3 >75% Sorted Int Test
+				
+				//Challenge 4 int[1000][1000] Rand Test 
+				
+				//Challenge 5 1D Array Mystery Comparables
+			}
 		//challengeOne: Standard sort and process
 		public abstract int challengeOne(int[] arr) 
 		{
