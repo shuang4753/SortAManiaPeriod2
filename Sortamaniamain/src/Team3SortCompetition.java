66cb0e3e1b20901df370840bdd53937ca59a0669
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-02-27 09:12:08 -0500

    hellos

--- Sortamaniamain/src/Team3SortCompetition.java
+++ Sortamaniamain/src/Team3SortCompetition.java
@@ -1,4 +1,21 @@
 
 public abstract class Team3SortCompetition extends SortCompetition {
+	public Team3SortCompetition(){
+		
+	}
+	public static int challengeOne(int[] arr){
+		for()
+	}
+	public static int challengeTwo(){
 
+	}
+	public static int challengeThree(){
+
+	}
+	public static int challengeFour(){
+
+	}
+	public static int challengeFive(){
+
+	}
 }
