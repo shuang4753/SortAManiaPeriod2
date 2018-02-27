a31fa781af08c8d0b495f86d1741e006cb274e4c
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-02-27 09:29:35 -0500

    asd

---
+++ Sortamaniamain/src/Runner.java
@@ -0,0 +1,27 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		int [] something1 = {1,3,2,4};
+	
+		SortCompetition something = new Team11SortCompetition();
+		
+		System.out.println(something.challengeOne(something1));
+		print(something1);
+	}
+	public static void print(int [] arr)
+	{
+		for (int i = 0; i < arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public int[] tenThousand(int []list1)
+	{
+		for (i = 0; i<list1.length; i++)
+		{
+			list1[i]=rand
+		}
+	}
+}
--- Sortamaniamain/src/Team11SortCompetition.java
+++ Sortamaniamain/src/Team11SortCompetition.java
@@ -4,38 +4,38 @@ public class Team11SortCompetition extends SortCompetition {
 
 	@Override
 	public int challengeOne(int[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+		quickSort(arr, 0, arr.length-1);
+		return ( arr[arr.length/2] + arr[(arr.length/2)-1]) / 2;
 	}
 
 	@Override
 	public int challengeTwo(String[] arr, String query) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public int challengeThree(int[] arr) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public int challengeFour(int[][] arr) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public int challengeFive(Comparable[] arr, Comparable query) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public String greeting() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "hi";
 	}
 
 	//Sorting Methods
