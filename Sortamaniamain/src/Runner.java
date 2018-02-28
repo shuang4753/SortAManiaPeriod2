0430a9ab7a11e537141e58210edf6c7d9a9920a1
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-28 09:20:03 -0500

    classes

---
+++ Sortamaniamain/src/Runner.java
@@ -0,0 +1,29 @@
+
+public class Runner 
+{
+	//Testing Code
+	public static void main(String [] args)
+	{
+		
+		//Challenge 1 10000 Rand Int Test
+		long start = System.nanoTime();
+		challengeOne();
+		long end = System.nanoTime();
+		long time = end - start;
+		
+		//Challenge 2 10000 Rand String Test
+		
+		//Challenge 3 >75% Sorted Int Test
+		
+		//Challenge 4 int[1000][1000] Rand Test 
+		
+		//Challenge 5 1D Array Mystery Comparables
+	}
+	
+	public static int[] randInts(int range);
+	{
+		
+	}
+	
+	
+}
