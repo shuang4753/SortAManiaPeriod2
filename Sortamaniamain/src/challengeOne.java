40f5ffb8524b80a79bc25d6d4dfd4ebb9f505d8f
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2018-03-02 09:27:11 -0500

    sdgjkes

--- Sortamaniamain/src/challengeOne.java
+++ Sortamaniamain/src/challengeOne.java
@@ -1,9 +1,29 @@
+import java.util.Random;
+import java.util.Arrays;
 
 public class challengeOne 
 {
 	
 	//testing code, delete later
-	
+	public static void main(String[] args)
+	{
+		int[] test = randomInts(20);
+		System.out.println(Arrays.toString(test));
+		bubbleSort(test);
+		System.out.println(Arrays.toString(test));
+		int med = test.length/2;
+		System.out.println(test[med]);
+	}
+	public static int[] randomInts(int lim)
+	{
+		int[]randArr = new int[lim];
+		Random a = new Random();
+		for(int x = 0; x < lim; x++)
+		{
+			randArr[x] = a.nextInt(lim);
+		}
+		return randArr;
+	}
 	
 	public static void bubbleSort(int[] list1)
 	{
@@ -18,8 +38,7 @@ public class challengeOne
 				}
 			}
 		}
-		
-		int med = list1.length/2;
+
 	
 	}
 	
