258fb9ebfeea48c17c35c425625864bc634e428e
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-03-02 09:14:45 -0500

    asukdfvlasdflasddflkasdfklasdhfliasdhf

---
+++ Sortamaniamain/src/Challenge2.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+
+public class Challenge2 extends SortCompetition
+{
+	public static String[] mergeSort(String[] S)
+	{
+		if(S.length >= 2)
+		{
+			String[] s1 = new String[S.length/2];
+			s1 = Arrays.copyOfRange(S, 0, (S.length/2)-1);
+			String[] s2 = new String[S.length-S.length/2];
+			s2 = Arrays.copyOfRange(S, S.length/2, S.length-1);
+			return merge(mergeSort(s1), mergeSort(s2));
+		}
+		else
+		{
+			return S;
+		}
+	}
+	
+	public static String[] merge(String[] x, String[] y)
+	{
+		String[] combo = new String[x.length + y.length];
+		int xc = 0;
+		int yc = 0;
+		while(xc + yc < (x.length + y.length) - 1)
+		{
+			if(x[xc].compareTo(y[yc])>=0)
+			{
+				combo[xc + yc] = y[yc];
+				yc++;
+			}
+			if(x[xc].compareTo(y[yc])<0)
+			{
+				combo[xc + yc] = x[xc];
+				xc++;
+			}
+		}
+		return combo;
+	}
+	@Override
+	public int challengeOne(int[] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	
+	public int challengeTwo(String[] arr, String query) 
+	{
+		mergeSort(arr);
+		for (int i = 0; i < arr.length; i++) 
+		{
+			if (arr.equals(query)) 
+			{
+			return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public int challengeThree(int[] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeFour(int[][] arr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int challengeFive(Comparable[] arr, Comparable query) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String greeting() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
