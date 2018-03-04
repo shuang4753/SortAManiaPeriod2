5e304006824abbb7fb4ebf5e5313abefddfcddca
Steven <Steven@Steven>
2018-03-04 17:26:42 -0500

    Challengetwo-basic

--- Sortamaniamain/src/Team11SortCompetition.java
+++ Sortamaniamain/src/Team11SortCompetition.java
@@ -15,7 +15,7 @@ public class Team11SortCompetition extends SortCompetition {
 		
 		int something = 0;
 		String [] result = mergeSort(arr);
-		for(int i = 0; i < arr.length; i++)
+		for(int i = arr.length -1; i >= 0; i++)
 		{
 			arr[i] = result[i];
 			if (arr[i].equals(query))
