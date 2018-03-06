4ccb39fbef0f593765260440d092aa5baf2721f7
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-03-06 09:20:31 -0500

    aa

--- Sortamaniamain/src/Challenge4.java
+++ Sortamaniamain/src/Challenge4.java
@@ -1,4 +1,17 @@
 //Lingli's class
-public class Challenge4 {
+import java.util.Arrays;
 
+public class Challenge4 {
+	public static void main(String[] args)
+	{
+		int[] randArr= randomInts(1000);
+		long start = System.nanoTime();
+		challengeFour(randArr);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Challenge 4 took: "+ time + " nanoseconds");
+		System.out.println(Arrays.toString(randArr));
+
+	}
+	
 }
