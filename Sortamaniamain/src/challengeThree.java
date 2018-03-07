2b98debd8af7ac5dab963e455a4b65705dc15c93
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-03-07 08:11:41 -0500

    insertion sort for challenge three

--- Sortamaniamain/src/challengeThree.java
+++ Sortamaniamain/src/challengeThree.java
@@ -1,5 +1,21 @@
-
 public class challengeThree 
 {
 
+	public static void insertionSort(int[] list1)
+	{
+		for(int x = 1; x < list1.length; x++)
+		{
+			int z = list1[x];
+			int y = x - 1;
+			while(y >= 0 && list1[y] > z) 
+			{
+				list1[y+1] = list1[y];
+				y = y - 1;
+			}
+			list1[y+1] = z;
+		}
+	}
+	
+	
+	
 }
