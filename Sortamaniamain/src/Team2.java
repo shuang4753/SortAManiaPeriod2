1b8c0e0138662ac3be1c373e3d79de5308cb3291
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-03-07 17:20:55 -0500

    dsdsds

--- Sortamaniamain/src/Team2.java
+++ Sortamaniamain/src/Team2.java
@@ -7,13 +7,13 @@ import java.util.Arrays;
  * 2/26/18
  */
 
-public class Team2 {
+public class Team2 extends SortCompetition{
 
-	public class Team2 extends SortCompetition
-	{
 		public static void main(String [] args)
 		{
-			
+
+			String [] test1 = {"apple", "cucum", "zorro", "zorro", "micro"};
+			String [] test2 = {"any", "can", "this", "body", "do"};
 			//Challenge 1 10000 Rand Int Test
 				
 			int[] randArr= randomInts(1000);
@@ -25,16 +25,98 @@ public class Team2 {
 			System.out.println(Arrays.toString(randArr));
 				
 				//Challenge 2 10000 Rand String Test
-				
+			start = System.nanoTime();
+			int indexReturned = challengeTwo("mincro",test1);
+			end = System.nanoTime();
+			time = end - start;
+			System.out.println("String Sort test took: "+ time + " nanoseconds");
+			System.out.println(Arrays.toString(test1)); 
+			System.out.println(indexReturned);	
 				//Challenge 3 >75% Sorted Int Test
 				
 				//Challenge 4 int[1000][1000] Rand Test 
 				
-				//Challenge 5 1D Array Mystery Comparables
+			//Challenge 5 1D Array Mystery Comparables
+			start = System.nanoTime();
+			indexReturned = challengeFive("can",test2);
+			end = System.nanoTime();
+			time = end - start;
+			System.out.println("Comparable test took: "+ time + " nanoseconds");
+			System.out.println(Arrays.toString(test2)); 
+			System.out.println(indexReturned);
 		}
+		
+		//challenge one
+		public static int challengeOne(int[] arr) 
+		{
+			//random number generator 
+			//data set: an array of 10,000 random integers between 0-1000
+			insertionSort(arr);
 			
-	}
-	
+			int middle = (arr.length/2);
+
+		    if (arr.length%2==1) 
+		    {
+		    	return arr[middle];
+		    } 
+		    
+		    else 
+		    {
+		    	return (arr[middle-1] + arr[middle])/2;	
+		    }
+		}	
+		
+		//challenge two
+		public static int challengeTwo (String given, String[] test1) {
+			stringSort(test1);
+			
+			for(int i = 0; i < test1.length; i++) {
+				if(test1[i].equals(given)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+		//challenge three
+		public static int[] challengeTree(int [] list1)
+		{
+			int x = list1.length;
+		
+			for(int i = 1 ; i < x; i++)
+			{
+				int y = list1[i];
+				int z = i - 1;
+				
+				while(z >= 0 && list1[z] > y)
+				{
+					list1[z + 1] = list1[z];
+					z = z - 1;
+				}
+				list1[z + 1] = y;
+			}
+			return list1;
+		}
+		
+		//challenge four
+		public static int[] challengeFour() {
+			//sort rows, for each sort, take the median value so u make a 1d array of medians (should be 100)
+			//return median number in the median row//
+		}
+		
+		//challenge five
+		public static int challengeFive (Comparable given, Comparable[] mysArr) {
+			cstringSort(mysArr);
+			for(int i = 0; i < mysArr.length; i++) {
+				if(mysArr[i].equals(given)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		
+// _______________________________________________________________________Private challenge 1
+		
 	private static int[]randomInts(int len)
 	{
 		int[] list1 = new int[len];
@@ -45,25 +127,6 @@ public class Team2 {
 		}
 	
 		return list1;
-	}
-	//challengeOne: Standard sort and process
-	public static int challengeOne(int[] arr) 
-	{
-		//random number generator 
-		//data set: an array of 10,000 random integers between 0-1000
-		insertionSort(arr);
-		
-		int middle = (arr.length/2);
-
-	    if (arr.length%2==1) 
-	    {
-	    	return arr[middle];
-	    } 
-	    
-	    else 
-	    {
-	    	return (arr[middle-1] + arr[middle])/2;	
-	    }
 	}
 	
 	private static void insertionSort(int[] arr)
@@ -84,8 +147,68 @@ public class Team2 {
 						break;
 					}
 				}
+		}
+	}
+	// Challenge 2 and 5 helper method
+	private static void cstringSort (Comparable[] test1) {
+		int swapNum = 0;
+		for(int i = 0; i<test1.length;i++) {
+			swapNum = i; 
+			for(int j=i-1; j>=0;j--) {
+				if(test1[swapNum].compareTo(test1[j])<0) {
+					cstringSwap(test1, swapNum, j);
+					swapNum--;
+				}
+				else if(test1[swapNum].compareTo(test1[j])>0) {
+					break;
+				}
 			}
 		}
+	}
+	
+	private static void cstringSwap(Comparable[]arr, int index1, int index2) {
+		Comparable string = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = string;
+	}
+	
+	private static void stringSort (String[] test1) {
+		int swapNum = 0;
+		for(int i = 0; i<test1.length;i++) {
+			swapNum = i; 
+			for(int j=i-1; j>=0;j--) {
+				if(test1[swapNum].compareTo(test1[j])<0) {
+					stringSwap(test1, swapNum, j);
+					swapNum--;
+				}
+				else if(test1[swapNum].compareTo(test1[j])>0) {
+					break;
+				}
+			}
+		}
+	}
+	
+	private static void stringSwap(String[]arr, int index1, int index2) {
+		String string = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = string;
+	}
+
+// ____________________________Private challenge 3
+	
+	public static int median(int [] list1)
+	{
+		int median = list1.length/2;
+		
+		if(list1.length % 2 == 1)
+		{
+			return list1[median]; 
+		}
+		else
+		{
+			return (list1[list1.length / 2] + ( list1[list1.length / 2 - 1])/2);
+		}
+	}
 
 }
 
