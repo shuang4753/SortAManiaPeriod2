10a7e52f1d0b998ff087bb0eaefda63f88465094
jwu9201 <32456209+jwu9201@users.noreply.github.com>
2018-03-07 09:28:21 -0500

    dasfwerqwer

--- Sortamaniamain/src/Main.java
+++ Sortamaniamain/src/Main.java
@@ -8,17 +8,43 @@ public class Main {
 		//Testing arrays
 				int [] test1 = {5,4,4,7,2,6,34,324,2,1,5,5357,2,54};
 				String [] test3 = {"zebra" , "tortilla" , "abba" , "foo", "bar", "aba"};
+				int[] test2 = {1,2,3,4,5,6,7,8,9,10,10,9,8,7,5};
 				Random rand = new Random();
 				int count = 50;
 				int[] One = new int[count];
 				for (int i = 0; i < count; i++) {
 					One[i] = rand.nextInt(count);
 				}
+				
 				int[] Two = One;
 				int[] Three = One;
 				int[] Four = One;
 				Team7 sh = new Team7();
+				
+				long start = System.nanoTime();
+				sh.challengeThree(test2);
+				long end = System.nanoTime();
+				long time = end -start; 
+				System.out.println(time + " nanoseconds");
+				
+				
+				
+				System.out.println(sh.challengeThree(test2));
+				System.out.println(Arrays.toString(test2));
+				
 
+				start = System.nanoTime();
+				sh.challengeTwo(test3, "s");
+				 end = System.nanoTime();
+				 time = end -start; 
+				System.out.println(time + " nanoseconds");
+				
+				System.out.println(sh.challengeTwo(test3, "zebra"));
+				System.out.println(Arrays.toString(test3));
+				
+
+				
+			/*
 				//Selection Sort Test
 				long start = System.nanoTime();
 				Team7.selectionSort(One);
@@ -30,10 +56,10 @@ public class Main {
 				
 				//Insertion Sort Test
 				 start = System.nanoTime();
-				Team7.challengeThree(Three);
+				Team7.insertionSort(Three);
 				 end = System.nanoTime();
 				 time = end -start; 
-				System.out.println("Insertion took: " + time + "nanoseconds");
+				System.out.println("Test2 took: " + time + "nanoseconds");
 				System.out.println(Arrays.toString(Three));
 				
 				
@@ -48,7 +74,7 @@ public class Main {
 
 				Team7.selectionSort2(test3);
 				
-
+*/
 				
 
 				
