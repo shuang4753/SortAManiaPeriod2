6b5c935baed3d8a2fa59cf42873b3dbff01d0440
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-07 09:00:39 -0500

    Challenge 1 started

---
+++ Sortamaniamain/src/Challenge_1.java
@@ -0,0 +1,33 @@
+//Mamadou Temp
+public abstract class Challenge_1 extends SortCompetition {
+	public static int Challenge_1(int[]numba)
+	{
+		return FindMedian(numba);	
+		System.out.println(x);
+	}
+	
+	
+	public static int FindMedian(int[]numba)
+	{
+		int median = numba[numba.length/2];
+		if(numba.length%2 == 1)
+		{
+			return ((numba[median]+numba[median+1])/2);
+		}
+		else return (numba[median]);
+	}
+	int[]test1 = {6729, 5894, 4335, 8867, 383, 519, 2244, 
+				  4615, 3251, 1398, 7511, 5453, 1234, 4297, 
+				  2459, 4990, 3069, 5926, 7864, 196, 8136, 4364};
+	int[]medtest = {1, 2, 3, 4, 5};
+	int[]medtest1 = {1, 2, 3, 4, 5, 6};
+	
+	int x = FindMedian(medtest);
+	int y = FindMedian(medtest1);
+
+	
+	
+
+}
+
+
