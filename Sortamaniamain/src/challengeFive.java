fa1a790255af7c5e43b0c70a08650c6575492ab6
Joyin <Joyin@192.168.0.110>
2018-03-07 20:52:16 -0500

    :(

--- Sortamaniamain/src/challengeFive.java
+++ Sortamaniamain/src/challengeFive.java
@@ -1,6 +1,14 @@
-
-public class challengeFive 
+import java.util.*;
+public class challengeFive implements Comparator<Object>
 {
+
+	@Override
+	public int compare(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 //help
 	//https://www.tutorialspoint.com/java/java_using_comparator.htm
 	//https://stackoverflow.com/questions/2839137/how-to-use-comparator-in-java-to-sort
